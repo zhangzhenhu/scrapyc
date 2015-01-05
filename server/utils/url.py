4893c1ad37e2af0b6665186a8038afb96ce4ee72
zhangzhenhu <acmtiger@gmail.com>
2015-01-05 11:55:00 +0800

    dev

--- server/utils/url.py
+++ server/utils/url.py
@@ -238,3 +238,37 @@ def _parse_keyvalue_args(name):
         return URL(name, *opts)
     else:
         return None
+
+
+
+from __future__ import with_statement
+from urlparse import urlparse
+
+# load tlds, ignore comments and empty lines:
+
+
+def get_domain(url):
+    with open("TLD.txt") as tld_file:
+        tlds = [line.strip() for line in tld_file if line[0] not in "/\n"]
+
+    url_elements = urlparse(url)[1].split('.')
+    # url_elements = ["abcde","co","uk"]
+
+    for i in range(-len(url_elements), 0):
+        last_i_elements = url_elements[i:]
+        #    i=-3: ["abcde","co","uk"]
+        #    i=-2: ["co","uk"]
+        #    i=-1: ["uk"] etc
+
+        candidate = ".".join(last_i_elements) # abcde.co.uk, co.uk, uk
+        wildcard_candidate = ".".join(["*"] + last_i_elements[1:]) # *.co.uk, *.uk, *
+        exception_candidate = "!" + candidate
+
+        # match tlds: 
+        if (exception_candidate in tlds):
+            return ".".join(url_elements[i:]) 
+        if (candidate in tlds or wildcard_candidate in tlds):
+            return ".".join(url_elements[i-1:])
+            # returns "abcde.co.uk"
+
+    raise ValueError("Domain not in global list of TLDs")

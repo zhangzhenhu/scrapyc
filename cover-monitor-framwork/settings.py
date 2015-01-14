71167df90281c0124d8b07c5126f954f970108a5
zhangzhenhu <acmtiger@gmail.com>
2015-01-14 13:28:39 +0800

    dev

--- cover-monitor-framwork/settings.py
+++ cover-monitor-framwork/settings.py
@@ -14,8 +14,8 @@ COMPONENTS = {
     "component.wiseccdb.WiseCcdb" :None,
     "component.l2linkbase.L2base" :None,
     "component.l2patch.L2Patch" :None,
-    "component.levelselect.LevelSelect" :None,
-    "component.levelall.LevelAll" :None,
+    #"component.levelselect.LevelSelect" :None,
+    #"component.levelall.LevelAll" :None,
     # "component.dc" :None,
 } 
 AFTER_STRATEGIES = [

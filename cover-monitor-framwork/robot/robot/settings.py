e9c307cfddc7952f49f3c52ce2a50f215fce8b01
zhangzhenhu <acmtiger@gmail.com>
2015-01-17 19:49:21 +0800

    dev

--- cover-monitor-framwork/robot/robot/settings.py
+++ cover-monitor-framwork/robot/robot/settings.py
@@ -7,7 +7,7 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
-
+import os
 BOT_NAME = 'robot'
 
 SPIDER_MODULES = ['robot.spiders']
@@ -15,6 +15,6 @@ NEWSPIDER_MODULE = 'robot.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'User-Agent:Mozilla/5.0 (Linux;u;Android 2.3.7;zh-cn;) AppleWebKit/533.1 (KHTML,like Gecko) Version/4.0 Mobile Safari/533.1 (compatible; +http://www.baidu.com/search/spi_der.html)'
-FEED_URI = 'file://out/%(name)s/%(time)s.json'
+FEED_URI = 'file://'+os.path.join(os.getcwd(),"feeds/%(name)s/%(time)s.json")
 FEED_FORMAT = 'jsonlines'
 FEED_STORE_EMPTY=True

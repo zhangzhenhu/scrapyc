88afece3a0217195b28efb062653f2c83e79a494
zhangzhenhu <acmtiger@gmail.com>
2015-01-19 11:29:31 +0800

    update

--- cover-monitor-framwork/id_wise.sh
+++ cover-monitor-framwork/id_wise.sh
@@ -11,7 +11,7 @@ SAMPLE_COUNT_URL=1500
 function get_query
 {
 rm ${DATA_PATH}/query.raw
-${HADOOP_BIN}  fs -get hdfs://szjjh-dbuild-hdfs.dmop.baidu.com:54310/user/score-int/zhangyi12/wise/round1/`date -d "1 day ago" +%Y%m%d-%Y%m%d`/ID/part-00000 ${DATA_PATH}/query.raw
+${HADOOP_BIN}  fs -get hdfs://szjjh-dbuild-hdfs.dmop.baidu.com:54310/user/score-int/zhangyi12/wise/round1/`date -d "2 days ago" +%Y%m%d-%Y%m%d`/ID/part-00000 ${DATA_PATH}/query.raw
 
 if [ $? -ne 0 ];then
 exit 1
@@ -26,7 +26,7 @@ local urlf=$1
 local wait_time=$2
 cd ./client
 rm ./sendpack.log
-rm -fr ./out_dir/* 
+rm -fr ./out_dir/317/0 
 ./cs_client -n  -c client_hk.conf -f ${urlf}
 sleep ${wait_time} 
 #${SAMPLE_COUNT_URL}+60
@@ -73,7 +73,18 @@ hot
 python run.py -i ${DATA_PATH}/url.hot.sample  -u "Ó¡ÄáÎÞÏß£¨ÈÈÃÅ£©¸²¸ÇÂÊ¼à¿Ø"  -d ./history/id-id/`date +%Y%m%d`/hot/ 1>${DATA_PATH}/cover_hot.log  2>&1 &
 
 random
-python run.py -i ${DATA_PATH}/url.random.sample -u "Ó¡ÄáÎÞÏß£¨Ëæ»ú£©¸²¸ÇÂÊ¼à¿Ø"   -d ./history/id-id/`date +%Y%m%d`/random/ 1>${DATA_PATH}/cover_random.log  2>&1
+python run.py -i ${DATA_PATH}/url.random.sample -u "Ó¡ÄáÎÞÏß£¨Ëæ»ú£©¸²¸ÇÂÊ¼à¿Ø"   -d ./history/id-id/`date +%Y%m%d`/random/ 1>${DATA_PATH}/cover_random.log  2>&1 &
+
+
+mkdir ./robot/data/`date +%Y%m%d`/
+robot_input=robot/data/`date +%Y%m%d`/url.list
+awk -F '\t'  '{print $1 }' ${DATA_PATH}/url.random.all  ${DATA_PATH}/url.hot.all | sort|uniq > ${robot_input}
+cd ./robot/
+scrapy crawl one -sJOBDIR=./data/`date +%Y%m%d`/job  -sinput=./data/`date +%Y%m%d`/url.list >log.txt 2>&1 &
+
+mkdir -p ./intbogus/
+cp ${robot_input} ./intbogus/`date +%Y%m%d`.google
+cat robot/feeds/one/*.json  | python utils/parsejson.py >> ./intbogus/`date +%Y%m%d`.google
 
 wait
 

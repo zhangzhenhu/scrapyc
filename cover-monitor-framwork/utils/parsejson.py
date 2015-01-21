b31e130fd975e626497946c5ab7c56026e740937
zhangzhenhu <acmtiger@gmail.com>
2015-01-21 10:14:01 +0800

    dev

--- cover-monitor-framwork/utils/parsejson.py
+++ cover-monitor-framwork/utils/parsejson.py
@@ -1,7 +1,18 @@
 import json
 import sys
 
+
+def get_url_schema(url):
+    if "://" in url[:10]:
+        return url[:10].split("://")[0]
+    return ""
+
 for line in sys.stdin:
-    item = json.loads(line.strip())
-    print item["url"].encode("utf8")
+    try:
+        item = json.loads(line.strip())
+        url = item["url"].encode("utf8")
+        if get_url_schema(url) not in ["http","https"]:
+            continue
     #print item["fromurl"]
+    except:
+        pass

65568f0bef1206ccb5727afce222b5bc128901e8
zhangzhenhu <acmtiger@gmail.com>
2015-02-05 09:33:09 +0800

    dev

--- cover-monitor-framwork/strategy/robots.py
+++ cover-monitor-framwork/strategy/robots.py
@@ -8,10 +8,10 @@ class Robots(Strategy):
         for case in data:
             if case.close:
                 continue
-        robots = case.get_data("robots")
-        if  not robots or robots.get('robots') != "DISALLOW":
-            return
-        case.set_result("conclusion","robots")
-        case.set_result("reason","robots=DISALLOW")
-        case.close = True
+            robots = case.get_data("robots")
+            if  not robots or robots.get('robots') != "DISALLOW":
+                return
+            case.set_result("conclusion","robots")
+            case.set_result("reason","robots=DISALLOW")
+            case.close = True
         return                

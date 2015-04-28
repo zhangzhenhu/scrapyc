b1d68e586dfed74af50e24fb2a2e9f624837df69
zhangzhenhu <acmtiger@gmail.com>
2015-04-28 10:10:31 +0800

    dev

--- server/projects/robot/robot/settings.py
+++ server/projects/robot/robot/settings.py
@@ -31,7 +31,7 @@ MAX_RUN_TASK = 10
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'robot (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36'
 EXTENSIONS = {
 #'scrapyc.server.utils.logstats.LogStats':200,
 }
@@ -39,11 +39,10 @@ ALLOW_SITES=[]
 FEED_URI="file:///home/spider/zhangzhenhu/robot/%(name)s/%(time)s.json"
 FEED_FORMAT="jsonlines"
 MAX_DEPTH = 2
-DEPTH_LIMIT=2
+DEPTH_LIMIT = 20
 DEPTH_PRIORITY = 1
 #DOWNLOAD_HANDLERS={'http':'scrapyc.server.utils.PhantomJSDownloadHandler.PhantomJSDownloadHandler',}
 WEBSERVICE_PORT=9805
 WEBSERVICE_ENABLED=1
-
 
 INPUT_FILE=None

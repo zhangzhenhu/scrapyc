42dcc3e1af54442fee50f85baf376d521dc7c026
zhangzhenhu <acmtiger@gmail.com>
2015-07-13 10:10:11 +0800

    dev

---
+++ server/projects/robot/robot/spiders/myapp-com.py
@@ -0,0 +1,52 @@
+import scrapy
+from scrapy.utils.response import get_base_url
+from w3lib.url import urljoin_rfc
+
+from scrapy import signals
+from robot.items import NimeiItem
+from robot.spiders import base
+from scrapy.utils.misc import load_object
+from scrapyc.server.utils.url import get_url_site,get_url_scheme
+import json
+
+
+class RobotSpider(base.RobotSpider):
+    name = "android.myapp.com"
+    allowed_domains = []
+    start_urls = [    ]
+    parses = {}
+
+    def start_requests(self):
+
+        yield scrapy.Request("http://android.myapp.com/myapp/cate/appList.htm?orgame=1&categoryId=0&pageSize=100&pageContext=1",callback=self.parse)
+        yield scrapy.Request("http://android.myapp.com/myapp/cate/appList.htm?orgame=2&categoryId=0&pageSize=100&pageContext=1",callback=self.parse)
+
+        for item in super(RobotSpider, self).start_requests():
+            yield item
+
+    def parse(self, response):
+        self.log("Crawled %s %d"%(response.url,response.status),level=scrapy.log.INFO)
+        #self.log("Crawled (%d) <GET %s>"%(response.status,response.url),level=scrapy.log.INFO)
+        if response.status / 100 != 2:
+            return
+        
+        res_data = json.loads(response.body)
+        for item in res_data["obj"]:
+            url = "http://android.myapp.com/myapp/detail.htm?apkName=%s"%(item["pkgName"])
+            yield self.baidu_rpc_request({"url":url,"src_id":4})
+            yield NimeiItem(url=url,furl=response.url)
+        if res_data["count"] ==100:
+            url = response.url
+            hehe = url.split("=")
+            hehe[-1] = int(hehe[-1])+100
+            url = "=".join(hehe)
+            yield scrapy.Request(url)
+
+
+
+
+    def spider_idle(self,spider):
+
+        if spider==self:
+            return False
+        return True

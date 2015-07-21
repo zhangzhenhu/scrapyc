0d863cdbe78a923f6361282382f240ddb1bde6b4
zhangzhenhu <acmtiger@gmail.com>
2015-07-21 15:36:15 +0800

    dev

--- server/projects/robot/robot/spiders/pdf.py
+++ server/projects/robot/robot/spiders/pdf.py
@@ -28,16 +28,22 @@ class RobotSpider(base.RobotSpider):
         if response.status / 100 != 2:
             return
         for a in response.xpath('//a'):
-            text = a.xpath("@text").extract()
+            text = a.xpath("u/text()").extract()
             if len(text) !=1:
                 continue
             text = text[0]
             if "PDF" not in text:
                 continue
-
+            href = a.xpath("@href").extract()
+            if len(href) != 1:
+                continue
+            href = href[0]
             if href == "#" or href.startswith("javascript") and len( a.xpath("@onclick").extract()) ==1:
                 onclick =  a.xpath("@onclick").extract()[0]
-                id = onclick.split(",")[1].replace("'","")
+                onclick = onclick.split(",")
+                if len(onclick) < 2:
+                    continue
+                id = onclick[1].split(")",1)[0].replace("'","")
                 pdf = response.url.split("/CN/",1)[0] + "/CN/article/downloadArticleFile.do?attachType=PDF&id="+id
             elif len( a.xpath("@href").extract()) ==1:
                 href = a.xpath("@href").extract()[0]
@@ -45,6 +51,6 @@ class RobotSpider(base.RobotSpider):
                 pdf = abs_url
 
             #url = "http://www.zjnyxb.cn/CN/article/downloadArticleFile.do?attachType=PDF&id="+id
-            print pdf
-            #yield self.baidu_rpc_request({"url":pdf,"src_id":4})                               
+            #print pdf
+            yield self.baidu_rpc_request({"url":pdf,"src_id":4})                             
 

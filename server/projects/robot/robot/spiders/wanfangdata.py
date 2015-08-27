7938fe1e060831dd8030f8a908aaf40d98022283
zhangzhenhu <acmtiger@gmail.com>
2015-08-27 11:03:11 +0800

    dev

--- server/projects/robot/robot/spiders/wanfangdata.py
+++ server/projects/robot/robot/spiders/wanfangdata.py
@@ -9,6 +9,7 @@ from scrapyc.server.utils.url import get_url_site,get_url_scheme
 import urlparse
 import re
 import datetime
+import string
 
 class RobotSpider(base.RobotSpider):
     name = "wanfangdata"
@@ -17,9 +18,11 @@ class RobotSpider(base.RobotSpider):
     start_urls = [    ]
     def start_requests(self):
 
-        yield scrapy.Request("http://c.wanfangdata.com.cn/Periodical.aspx")
-        yield scrapy.Request("http://c.wanfangdata.com.cn/LastUpdatedPeriodical.aspx")
-        # yield scrapy.Request("http://cyfd.cnki.com.cn/catenav.aspx")
+        # yield scrapy.Request("http://c.wanfangdata.com.cn/Periodical.aspx")
+        # yield scrapy.Request("http://c.wanfangdata.com.cn/LastUpdatedPeriodical.aspx")
+        for letter in string.ascii_uppercase:
+            url = "http://c.wanfangdata.com.cn/PeriodicalLetter.aspx?NodeID=%s"%letter
+            yield scrapy.Request(url,self.parse_index)
         # yield scrapy.Request("http://cdmd.cnki.com.cn/Area/CDMDUnitArticle-10183-2015-1.htm")
         # yield scrapy.Request("")
         # yield scrapy.Request("")
@@ -43,6 +46,46 @@ class RobotSpider(base.RobotSpider):
             if name not in rm_query and name :
                 n_query += "&%s=%s"%(name,value)
         return urlparse.urlunparse((up.scheme,up.netloc,up.path, up.params,n_query[1:],up.fragment))
+
+    def parse_index(self,response):
+        self.log("Crawled %s %d"%(response.url,response.status),level=scrapy.log.INFO)
+        if response.status / 100 != 2:
+            return
+        base_url  = get_base_url(response)
+        #解析期刊首页
+        for href in response.xpath("//div[@id='divperilist']/ul/li/a/@href").extract():
+            if href.startswith("Rss.ashx?"):
+                continue
+            relative_url = href
+            abs_url =urljoin_rfc(base_url,relative_url)
+            yield self.baidu_rpc_request({"url":abs_url,"src_id":4})
+            yield scrapy.Request(url=abs_url,callback=self.parse_content)
+
+        #解析索引页翻页
+        for href in response.xpath("//div[@id='divperilist']/table//a/@href").extract():
+            if "PageNo" not in href:
+                continue
+            relative_url = href
+            abs_url =urljoin_rfc(base_url,relative_url)
+            yield scrapy.Request(url=abs_url,callback=self.parse_index)
+            yield self.baidu_rpc_request({"url":abs_url,"src_id":4})
+
+    def parse_content(self,response):
+        self.log("Crawled %s %d"%(response.url,response.status),level=scrapy.log.INFO)
+        #self.log("Crawled (%d) <GET %s>"%(response.status,response.url),level=scrapy.log.INFO)
+        if response.status / 100 != 2:
+            return
+        base_url  = get_base_url(response)
+        #解析文章
+        for href in response.xpath("//div[@id='wrap3']//a[@class='qkcontent_name']/@href").extract():
+            yield self.baidu_rpc_request({"url":href,"src_id":4})
+
+        #解析历史期刊首页
+        for href in response.xpath("//div[@id='wrap3']//ul[@class='new_ul5']/li/p/a/@href").extract():
+            relative_url = href
+            abs_url =urljoin_rfc(base_url,relative_url)
+            yield scrapy.Request(url=abs_url,callback=self.parse_content)            
+            yield self.baidu_rpc_request({"url":abs_url,"src_id":4})
 
 
     def parse(self,response):

23a77034abfccf0b9f4fd941e39db4d34c8570e3
zhangzhenhu <acmtiger@gmail.com>
2015-10-28 10:17:36 +0800

    dev

--- server/projects/robot/robot/spiders/baidu-com.py
+++ server/projects/robot/robot/spiders/baidu-com.py
@@ -44,11 +44,12 @@ class BaiduSpider(scrapy.Spider):
         if response.__class__ != scrapy.http.HtmlResponse:
             return
         query = url_query_parameter(response.url, 'wd')
+        print query,
         for json_str in response.xpath("//div/@data-click").extract():
             if "rsv_re_ename" in json_str and "rsv_re_uri" in json_str:
                 data = json.loads(json_str.replace("'", '"'))
-                print query + "\t" + data["rsv_re_ename"].encode("gb18030") + "\t" + data["rsv_re_uri"].encode("gb18030")
-
+                print "\t" + data["rsv_re_ename"].encode("gb18030") + "\t" + data["rsv_re_uri"].encode("gb18030")
+        print
     def is_valid_url(self, url):
         if url.startswith("javascript:") or url.startswith("mailto:") or url == "#":
             return False

4daf80df237e4aa4303b35287f8c9fade6ec9538
zhangzhenhu <acmtiger@gmail.com>
2015-11-27 15:28:25 +0800

    dev

--- projects/mtime/mtime/pipelines.py
+++ projects/mtime/mtime/pipelines.py
@@ -4,7 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.exporters import BaseItemExporter
+try:
+    from scrapy.exporters import BaseItemExporter
+except:
+    from scrapy.contrib.exporter import BaseItemExporter
+
 
 class NimeiPipeline(object):
     def process_item(self, item, spider):
